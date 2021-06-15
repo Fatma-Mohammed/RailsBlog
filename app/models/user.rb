a8dc7ae99951ccd6c16ad46e40bcb44b8cfc165c
Fatma Mohammed <fatma.mohammed@nezam.io>
2021-06-15 12:39:41 +0200

    Validate email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
     mount_uploader :image, ImageUploader
     has_many :posts
     has_many :comments
+    before_validation :downcase_email
+    validates :email, uniqueness: true
+
+    private
+    
+    def downcase_email
+      self.email = email.downcase if email.present?
+    end
+    
 end
