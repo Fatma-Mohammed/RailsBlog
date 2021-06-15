9eba242c7b0647fd0ffff40f8383ecde34935466
Fatma Mohammed <fatma.mohammed@nezam.io>
2021-06-15 12:27:06 +0200

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :users, only: [:create]
-  post '/login', to: 'users#login'
-  get 'auto_login', to: 'users#auto_login'
+  scope '/api' do
+    scope '/v1' do
+      post '/register',  to: 'users#create'
+      post '/login', to: 'users#login'
+      get 'auto_login', to: 'users#auto_login'
+      resources :posts
+      resources :posts, param: :id do
+        resources :comments
+      end
+      
+    end
+  end
 end
